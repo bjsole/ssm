e446fa0da4842bdea03c3fef2a5235fc26230894
bj <15735180845@163.com>
2019-07-19 17:43:54 +0800

    添加testController

---
+++ src/main/java/com/qf/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.qf.controller;
+
+/**
+ * @author bbj 2019/7/19 17:42
+ * @version 1.0
+ */
+public class TestController {
+    public static void main(String[] args) {
+
+        System.out.println("呵呵呵呵");
+
+    }
+}
